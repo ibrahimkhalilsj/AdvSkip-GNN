edad9a8bd1edcccf5affda6f77739bb24ffffbfe
MD IBRAHIM KHALIL <khalil_ibrahim@njust.edu.cn>
2024-07-02 19:22:26 +0800

    train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from __future__ import division    #
+from __future__ import division
 from __future__ import print_function
 import time
 import random
@@ -7,6 +7,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.autograd import Variable
 from utils import *
 from model import *
 import uuid
@@ -22,10 +23,10 @@ parser.add_argument('--layer', type=int, default=64, help='Number of layers.')
 parser.add_argument('--hidden', type=int, default=64, help='hidden dimensions.')
 parser.add_argument('--dropout', type=float, default=0.6, help='Dropout rate (1 - keep probability).')
 parser.add_argument('--patience', type=int, default=100, help='Patience')
-parser.add_argument('--data', default='cora', help='dateset')
+parser.add_argument('--data', default='cora', help='dataset')
 parser.add_argument('--dev', type=int, default=0, help='device id')
 parser.add_argument('--alpha', type=float, default=0.1, help='alpha_l')
-parser.add_argument('--lamda', type=float, default=0.5, help='lamda.')
+parser.add_argument('--lamda', type=float, default=0.5, help='lambda.')
 parser.add_argument('--variant', action='store_true', default=False, help='GCN* model.')
 parser.add_argument('--test', action='store_true', default=False, help='evaluation on test set.')
 args = parser.parse_args()
@@ -35,72 +36,83 @@ torch.manual_seed(args.seed)
 torch.cuda.manual_seed(args.seed)
 
 # Load data
-adj, features, labels,idx_train,idx_val,idx_test = load_citation(args.data)
-cudaid = "cuda:"+str(args.dev)
+adj, features, labels, idx_train, idx_val, idx_test = load_citation(args.data)
+cudaid = "cuda:" + str(args.dev)
 device = torch.device(cudaid)
 features = features.to(device)
 adj = adj.to(device)
-checkpt_file = 'pretrained/'+uuid.uuid4().hex+'.pt'
-print(cudaid,checkpt_file)
+checkpt_file = 'pretrained/' + uuid.uuid4().hex + '.pt'
+print(cudaid, checkpt_file)
 
 model = GCNII(nfeat=features.shape[1],
-                nlayers=args.layer,
-                nhidden=args.hidden,
-                nclass=int(labels.max()) + 1,
-                dropout=args.dropout,
-                lamda = args.lamda, 
-                alpha=args.alpha,
-                variant=args.variant).to(device)
+              nlayers=args.layer,
+              nhidden=args.hidden,
+              nclass=int(labels.max()) + 1,
+              dropout=args.dropout,
+              lamda=args.lamda,
+              alpha=args.alpha,
+              variant=args.variant).to(device)
 
 optimizer = optim.Adam([
-                        {'params':model.params1,'weight_decay':args.wd1},
-                        {'params':model.params2,'weight_decay':args.wd2},
-                        ],lr=args.lr)
+    {'params': model.params1, 'weight_decay': args.wd1},
+    {'params': model.params2, 'weight_decay': args.wd2},
+], lr=args.lr)
 
+# Function for adversarial example generation using FGSM
+def generate_adversarial_example(model, features, adj, labels, idx_train, epsilon=0.01):
+    model.train()
+    optimizer.zero_grad()
+
+    # Forward pass on the original data
+    output = model(features, adj)
+    loss = F.nll_loss(output[idx_train], labels[idx_train].to(device))
+    loss.backward()
+
+    # Generate adversarial example using FGSM
+    adversarial_features = features + epsilon * features.grad.sign()
+    adversarial_features = Variable(adversarial_features, requires_grad=True)
+
+    return adversarial_features
+
+# Modify the train() function to include adversarial training
 def train():
     model.train()
     optimizer.zero_grad()
-    output = model(features,adj)
+
+    # Original data
+    output = model(features, adj)
     acc_train = accuracy(output[idx_train], labels[idx_train].to(device))
     loss_train = F.nll_loss(output[idx_train], labels[idx_train].to(device))
-    loss_train.backward()
+
+    # Adversarial training
+    adversarial_features = generate_adversarial_example(model, features, adj, labels, idx_train)
+    adversarial_output = model(adversarial_features, adj)
+    adversarial_loss = F.nll_loss(adversarial_output[idx_train], labels[idx_train].to(device))
+
+    # Combined loss
+    total_loss = loss_train + adversarial_loss
+
+    total_loss.backward()
     optimizer.step()
-    return loss_train.item(),acc_train.item()
 
+    return total_loss.item(), acc_train.item()
 
-def validate():
-    model.eval()
-    with torch.no_grad():
-        output = model(features,adj)
-        loss_val = F.nll_loss(output[idx_val], labels[idx_val].to(device))
-        acc_val = accuracy(output[idx_val], labels[idx_val].to(device))
-        return loss_val.item(),acc_val.item()
-
-def test():
-    model.load_state_dict(torch.load(checkpt_file))
-    model.eval()
-    with torch.no_grad():
-        output = model(features, adj)
-        loss_test = F.nll_loss(output[idx_test], labels[idx_test].to(device))
-        acc_test = accuracy(output[idx_test], labels[idx_test].to(device))
-        return loss_test.item(),acc_test.item()
-    
 t_total = time.time()
 bad_counter = 0
 best = 999999999
 best_epoch = 0
 acc = 0
 for epoch in range(args.epochs):
-    loss_tra,acc_tra = train()
-    loss_val,acc_val = validate()
-    if(epoch+1)%1 == 0: 
-        print('Epoch:{:04d}'.format(epoch+1),
-            'train',
-            'loss:{:.3f}'.format(loss_tra),
-            'acc:{:.2f}'.format(acc_tra*100),
-            '| val',
-            'loss:{:.3f}'.format(loss_val),
-            'acc:{:.2f}'.format(acc_val*100))
+    loss_tra, acc_tra = train()
+    loss_val, acc_val = validate()
+    if (epoch + 1) % 1 == 0:
+        print('Epoch:{:04d}'.format(epoch + 1),
+              'train',
+              'loss:{:.3f}'.format(loss_tra),
+              'acc:{:.2f}'.format(acc_tra * 100),
+              '| val',
+              'loss:{:.3f}'.format(loss_val),
+              'acc:{:.2f}'.format(acc_val * 100))
     if loss_val < best:
         best = loss_val
         best_epoch = epoch
@@ -118,10 +130,4 @@ if args.test:
 
 print("Train cost: {:.4f}s".format(time.time() - t_total))
 print('Load {}th epoch'.format(best_epoch))
-print("Test" if args.test else "Val","acc.:{:.1f}".format(acc*100))
-    
-
-
-
-
-
+print("Test" if args.test else "Val", "acc.:{:.1f}".format(acc * 100))
