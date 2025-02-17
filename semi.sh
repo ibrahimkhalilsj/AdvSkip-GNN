e7a47dc0c713f7597f49d1dbe433d73e206dc7ed
MD IBRAHIM KHALIL <khalil_ibrahim@njust.edu.cn>
2025-02-18 03:16:52 +0800

    semi.sh

--- semi.sh
+++ semi.sh
@@ -1,4 +1,4 @@
-python -u train.py --data cora --layer 64 --test  #
+python -u train.py --data cora --layer 64 --test
 python -u train.py --data cora --layer 64 --variant --test
 python -u train.py --data citeseer --layer 32 --hidden 256 --lamda 0.6 --dropout 0.7 --test
 python -u train.py --data citeseer --layer 32 --hidden 256 --lamda 0.6 --dropout 0.7 --variant --test
